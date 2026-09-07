9db24ced70533cd1aba428bb634d2b6abcd073ad
Nikita Korobeinikov <korobeinikov@surf.dev>
2026-09-07 11:59:18 +0400

    Pin swift-snapshot-testing dependency to exact 1.18.12

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/NullIsOne/swift-snapshot-testing",
-            branch: "fix-snapshot-assertion-path"
+            exact: "1.18.12"
         ),
     ],
     targets: [
